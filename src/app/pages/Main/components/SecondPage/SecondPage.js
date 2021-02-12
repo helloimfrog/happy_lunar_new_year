88996088cb861ba4ccb4a8ea12263053b5b88e65
Nguyen Ngoc Long <princefrog2k@Nguyens-MacBook-Pro.local>
2021-02-12 14:05:04 +0700

    improve-Vinh-shitty-code

--- src/app/pages/Main/components/SecondPage/SecondPage.js
+++ src/app/pages/Main/components/SecondPage/SecondPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Spring } from 'react-spring/renderprops';
 import AppString from '../../../../utils/AppString';
 
 import './SecondPage.css';
@@ -8,26 +7,19 @@ export default class SecondPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      showFirework: true,
-      rotated: "",
       rotating: "",
       rotatingMainBox: "",
       rotatingArrows: "",
-      showWishZone: "none"
+      animShowWishZone: false
     }
     this.wishQuote = AppString.getRandomWish();
-  }
-
-  componentDidMount() {
-    setTimeout(() => this.setState({ showFirework: false }), 5000)
   }
 
   render() {
     return (
       <div className="main-view-of-2nd-screen">
-        {this._renderGiftBox()}      
-
-        <div className="wish-zone" style={{ height: window.innerHeight * 2 / 3, display:`${this.state.showWishZone}` }}>
+        {this._renderGiftBox()}
+        <div className={`wish-zone ${this.state.animShowWishZone ? "wish-zone-anim" : ""}`}>
           <img
             alt="blossom-top-left"
             className="blossom-top-left"
@@ -47,86 +39,50 @@ export default class SecondPage extends React.Component {
               style={{ maxHeight: 60, maxWidth: 60, marginRight: 10 }}
               src={require('../../../../assets/animal_of_year_2021.png')}
             />
-            <Spring
-              from={{ opacity: 0 }}
-              to={{ opacity: 1 }}
-              delay={1500}
-            >
-              {
-                props =>
-                  <span style={props}>{AppString.get("from_author")}</span>
-              }
-            </Spring>
+            <span>{AppString.get("from_author")}</span>
           </div>
-          <Spring
-            from={{ opacity: 0 }}
-            to={{ opacity: 1 }}
-            delay={1300}
-          >
-            {
-              props => <span className="wish-quote" style={props}>{this.wishQuote}</span>
-            }
-          </Spring>
+          <span className="wish-quote">{this.wishQuote}</span>
         </div>
       </div>
     );
   }
-  _animate(){
-    let isDone = this.state.rotated === "rotated" ? true : false;
-    let init_state= this.state;
-    if (!isDone) {
-      this.setState({ rotating: "rotating", rotatingMainBox: "rotating-down", rotatingArrows: "rotating-arrows" });
-      setTimeout(() => {
-        this.setState({ rotated: "rotated" }, () =>{
-          this.setState({
-            showWishZone: "flex",
-          })
-        });
-      }, 1000);
-    } else {
-      this.setState(init_state)
-    }
+
+  _animate() {
+    this.setState({
+      rotating: "rotating",
+      rotatingMainBox: "rotating-down",
+      rotatingArrows: "rotating-arrows",
+      animShowWishZone: true
+    });
   }
 
-  _renderGiftBox(){
-    const {rotated, rotating, rotatingMainBox, rotatingArrows} = this.state;
-    console.log(this.state)
-
-    return(
-      <div style={{
-
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
-        flexDirection: "column"
-      }}>
-        <div className={`wrapTextArrow ${rotatingArrows}`}>
-          <div className="textBox">
+  _renderGiftBox() {
+    const { rotating, rotatingMainBox, rotatingArrows } = this.state;
+    return (
+      <div className="wrap-text-arrow-giftbox">
+        <div className={`wrap-text-arrow ${rotatingArrows}`}>
+          <div className="text-label">
             {AppString.get('label_gift_box')}
           </div>
           <div className="arrows"></div>
         </div>
-        <div className="wrapGiftBox" onClick={() => this._animate()}>
-          <div className="lidGiftBox">
+        <div className="wrap-gift-box" onClick={this._animate.bind(this)}>
+          <div className="lid-gift-box">
             <img
-                alt="lidGiftBox__lid"
-                className={`lidGiftBox__lid ${rotating} ${rotated}`}
-                src={require('../../../../assets/lid_gift_box.png')}
-              />
+              alt="lid-gift-box__lid"
+              className={`lid-gift-box__lid ${rotating}`}
+              src={require('../../../../assets/lid_gift_box.png')}
+            />
           </div>
-          <div className="mainGiftBox">
+          <div className="main-gift-box">
             <img
-                alt="mainGiftBox__main"
-                className={`mainGiftBox__main ${rotatingMainBox}`}
-                src={require('../../../../assets/main_gift_box.png')}
-              />
+              alt="main-gift-box__main"
+              className={`main-gift-box__main ${rotatingMainBox}`}
+              src={require('../../../../assets/main_gift_box.png')}
+            />
           </div>
         </div>
       </div>
-
     )
   }
 }
