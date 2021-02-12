0937fccd849bc8c145033f25c1be6e076dad2a10
Nguyen Ngoc Long <princefrog2k@Nguyens-MacBook-Pro.local>
2021-02-12 11:56:20 +0700

    2021-page-welcome

--- src/app/pages/Main/components/FirstPage/FirstPage.js
+++ src/app/pages/Main/components/FirstPage/FirstPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Fireworks } from 'fireworks/lib/react';
-import { Spring } from 'react-spring/renderprops';
 import { MAX_SCREEN_MOBILE_WIDTH } from '../../../../utils/AppOtherValues';
 
 import './FirstPage.css';
@@ -9,6 +8,13 @@ export default class FirstPage extends React.Component {
   constructor() {
     super();
     this.isMobileViewport = window.innerWidth < MAX_SCREEN_MOBILE_WIDTH;
+    this.state = {
+      showFireworks: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ showFireworks: false }), 5000);
   }
 
   render() {
@@ -25,46 +31,17 @@ export default class FirstPage extends React.Component {
       })
     }
     return (
-      <div className="main-view" style={this.props.animationProps}>
-        <Spring
-          from={{ left: -50, opacity: 0 }}
-          to={{ left: 0, opacity: 1 }}
-          config={{ delay: 1500, duration: 200 }}
-        >
-          {props => <div style={props}><Fireworks {...fxProps} /></div>}
-        </Spring>
-        <Spring
-          from={{ left: -50, opacity: 0 }}
-          to={{ left: 0, opacity: 1 }}
-          config={{ delay: 500 }}
-        >
-          {props =>
-            <img
-              alt="blossom-top-left"
-              style={props}
-              className="blossom"
-              src={require('../../../../assets/blossom.png')}
-            />
-          }
-        </Spring>
-        <Spring
-          from={{ maxHeight: '50%', maxWidth: '50%', opacity: 0 }}
-          to={{
-            maxHeight: this.isMobileViewport ? '80%' : '40%',
-            maxWidth: this.isMobileViewport ? '80%' : '40%',
-            opacity: 1
-          }}
-          config={{ delay: 1000 }}
-        >
-          {props =>
-            <img
-              alt="2020-logo"
-              className="logo-2020"
-              style={props}
-              src={require('../../../../assets/2020_logo.png')}
-            />
-          }
-        </Spring>
+      <div className="main-view">
+        {
+          this.state.showFireworks
+            ? <Fireworks {...fxProps} />
+            : null
+        }
+        <img
+          alt="2020-logo"
+          className="logo-2020"
+          src={require('../../../../assets/2021_logo.png')}
+        />
       </div>
     );
   }
